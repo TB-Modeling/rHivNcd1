fb615679a0c4f944f55ad9d9fe7a91a8ff6354ec
mschnure <mschnur3@jhmi.edu>
2022-09-28 15:58:56 -0400

    removed TICK from initial NCD and HIV states; removed old functions

--- rCoreFunctions.R
+++ rCoreFunctions.R
@@ -37,12 +37,13 @@ get.hiv.probabilities = function(){
   hiv.probs
 }
 
+
+
 # model initial HIV status 
-set.initial.hiv.status = function(personID, #id of a selected population member
-                                  TICK #current time (should pass via the global parameter)
+set.initial.hiv.status = function(personID #id of a selected population member
+
 ){
   p<-pop[[personID]]
-  
   hiv.probs = get.hiv.probabilities()
   hiv.states = unlist(dimnames(hiv.probs)[1])
   specific.hiv.probs = hiv.probs[,p$sex,p$agegroup]
@@ -53,18 +54,12 @@ set.initial.hiv.status = function(personID, #id of a selected population member
     p$hivState=0
   if(hiv.status=="undiagnosed"){
     p$hivState=1
-    p$tHivInc=TICK
   }
   if(hiv.status=="diagnosed_unengaged"){
     p$hivState=2
-    p$tHivInc=TICK
-    tHivDiag=TICK
   }
   if(hiv.status=="engaged_unsuppressed" | hiv.status=="engaged_suppressed"){
     p$hivState=3
-    p$tHivInc=TICK
-    p$tHivDiag=TICK
-    p$tHivEng=TICK # old version did tHivSupp instead
   }
 
 }
@@ -80,82 +75,16 @@ create.initial.population = function(TICK){
   sim.pop$ncd[sim.pop$hypertension==1 & sim.pop$diabetes==0] = mc$NCD.HYP #2, hypertensive
   sim.pop$ncd[sim.pop$hypertension==1 & sim.pop$diabetes==1] = mc$NCD.DIAB_HYP #3, both
 
-  sim.pop$tDiabinc=NULL
-  sim.pop$tDiabInc[sim.pop$ncd==1 | sim.pop$ncd==3] = TICK
-  
-  sim.pop$tHypInc=NULL
-  sim.pop$tHypInc[sim.pop$ncd==3 | sim.pop$ncd==3] = TICK
-  
   vIds = sim.pop$X
   vAges = sim.pop$age
   vSexes = sim.pop$sex
   vncdState = sim.pop$ncd
-  vtDiabInc = sim.pop$tDiabInc
-  vtHypInc = sim.pop$tHypInc
 
-  pop = (mapply(Person$new, vIds,vSexes,vAges,TICK,vncdState,vtDiabInc,vtHypInc))
+
+  pop = (mapply(Person$new, vIds,vSexes,vAges,TICK,vncdState))
   pop = unlist(pop)
 
   pop
-}
-
-## OLD VERSIONS BELOW - I THINK THESE CAN BE REMOVED ## 
-
-## MS - I think I can remove this one now; but check other places for Supp (should now be Eng)
-#model initial HIV status
-set.initiatl.hiv.status<-function(personID, #id of a selected population member
-                                  TICK #current time (should pass via the global parameter)
-){
-  p<-pop[[personID]]
-  r<-runif(1)
-  mat<-cumsum(mat.hiv.initial.prevalence.ratio[p$sex,p$agegroup,])
-  #
-  # print(personID)
-  if (r< mat[mc$HIV.NEG+1]) {
-    p$hivState<-0
-  }else{
-    if (r<mat[mc$HIV.UNDIAG+1]){
-      p$hivState=mc$HIV.UNDIAG
-      p$tHivinc=TICK
-    }else{
-      if(r<mat[mc$HIV.DIAG_UNSUPP+1]){
-        p$hivState=mc$HIV.DIAG_UNSUPP
-        p$tHivinc=TICK
-        p$tHivdiag=TICK
-      }else{
-        p$hivState=mc$HIV.SUPP
-        p$tHivinc=TICK
-        p$tHivdiag=TICK
-        p$tHivsupp=TICK
-      }}}
-}
-
-
-## MS - I think I can remove this one now that I've added incidence times to create initial 
-#model initial NCD status
-set.initiatl.ncd.status<-function(personID,
-                                  TICK 
-){
-  p<-pop[[personID]]
-  r<-runif(1)
-  mat<-cumsum(mat.ncd.initial.prevalence.ratio[p$sex,p$agegroup,])
-  #
-  # print(personID)
-  if (r< mat[mc$NCD.NEG+1]) {
-    p$ncdState<-0
-  }else{
-    if (r<mat[mc$NCD.DIAB+1]){
-      p$ncdState=mc$NCD.DIAB
-      p$tDiabinc=TICK
-    }else{
-      if(r<mat[mc$NCD.HYP+1]){
-        p$ncdState=mc$NCD.HYP
-        p$tHypinc=TICK
-      }else{
-        p$ncdState=mc$NCD.DIAB_HYP
-        p$tHypinc=TICK
-        p$tDiabinc=TICK
-      }}}
 }
 
 
