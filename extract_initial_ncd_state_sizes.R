c6b470797cb4167480495d0ff4219d12e959ba0f
mschnure <mschnur3@jhmi.edu>
2023-03-23 11:02:54 -0400

    new ncd.state.sizes.2015.csv file

--- extract_initial_ncd_state_sizes.R
+++ extract_initial_ncd_state_sizes.R
@@ -2,13 +2,13 @@
 # I then saved it externally so that we read it into the model everytime. you can remove this after reviewing it
 #add agegroups
 step.dataset$agegroup[step.dataset$agegroup<1]<-1
-step.dataset$agegroup[step.dataset$agegroup>mc$DIM.AGE]<-mc$DIM.AGE
+step.dataset$agegroup[step.dataset$agegroup>DIM.AGE]<-DIM.AGE
 #add ncd state
 step.dataset$ncdstate=step.dataset$hypertension+2*step.dataset$diabetes+1
 #loop through and count the state sizes
 ncd.state.sizes<-array(0,
-                       dim=c(mc$DIM.AGE,mc$DIM.SEX,mc$DIM.NCD),
-                       dimnames = list(mc$DIM.NAMES.AGE,mc$DIM.NAMES.SEX,mc$DIM.NAMES.NCD))
+                       dim=c(DIM.AGE,DIM.SEX,DIM.NCD),
+                       dimnames = list(DIM.NAMES.AGE,DIM.NAMES.SEX,DIM.NAMES.NCD))
 invisible(lapply(1:nrow(step.dataset),function(x){
   p<-step.dataset[x,]
   ncd.state.sizes[p$agegroup,
